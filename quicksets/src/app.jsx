0eae76ae10867d7d9d08293c4ae3a1dd4861e3bb
SethHales <sethahales@gmail.com>
2026-02-05 20:09:46 -0700

    Header and footer are back to working!

--- quicksets/src/app.jsx
+++ quicksets/src/app.jsx
@@ -3,5 +3,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="app bg-dark text-light">App will display here</div>;
+    return (
+        <div className="app bg-dark text-light">
+            <header>
+                <img src="images/quicksets_logo.png" alt="QuickSets Logo" className="logo" />
+                <h1>History</h1>
+                <p>Ronald Weasley</p>
+            </header>
+
+            <main>App components go here</main>
+
+            <footer>
+                <nav className="tab-menu">
+                    <a href="log.html" className="tab">Logger</a>
+                    <a href="history.html" className="tab" id="active">History</a>
+                    <a href="profile.html" className="tab">Profile</a>
+                </nav>
+            </footer>
+        </div>
+    );
 }
