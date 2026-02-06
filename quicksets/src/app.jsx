608b20c89b7c917e9fb5be1c69c453fe3fa6659b
SethHales <sethahales@gmail.com>
2026-02-05 20:21:09 -0700

    Router is working!!

--- quicksets/src/app.jsx
+++ quicksets/src/app.jsx
@@ -2,24 +2,42 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { History } from './history/history';
+import { Logger } from './logger/logger';
+import { Profile } from './profile/profile';
+
 export default function App() {
     return (
-        <div className="app bg-dark text-light">
-            <header>
-                <img src="images/quicksets_logo.png" alt="QuickSets Logo" className="logo" />
-                <h1>History</h1>
-                <p>Ronald Weasley</p>
-            </header>
+        <BrowserRouter>
+            <div className="app bg-dark text-light">
+                <header>
+                    <img src="images/quicksets_logo.png" alt="QuickSets Logo" className="logo" />
+                    <h1>History</h1>
+                    <p>Ronald Weasley</p>
+                </header>
 
-            <main>App components go here</main>
+                <Routes>
+                    <Route path='/' element={<Login />} exact />
+                    <Route path='/history' element={<History />} />
+                    <Route path='/logger' element={<Logger />} />
+                    <Route path='/profile' element={<Profile />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
 
-            <footer>
-                <nav className="tab-menu">
-                    <a href="log.html" className="tab">Logger</a>
-                    <a href="history.html" className="tab" id="active">History</a>
-                    <a href="profile.html" className="tab">Profile</a>
-                </nav>
-            </footer>
-        </div>
+                <footer>
+                    <nav className="tab-menu">
+                        <NavLink to="/logger" className="tab">Logger</NavLink>
+                        <NavLink to="/history" className="tab" id="active">History</NavLink>
+                        <NavLink to="/profile" className="tab">Profile</NavLink>
+                    </nav>
+                </footer>
+            </div>
+        </BrowserRouter>
     );
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
---
+++ quicksets/src/logger/logger.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Logger() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>logger displayed here</div>
+    </main>
+  );
+}
