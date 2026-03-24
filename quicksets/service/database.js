90007c0b6f9c233548b8682abf6171352fc8c2ae
SethHales <sethahales@gmail.com>
2026-03-24 10:17:52 -0600

    Added database stuff

---
+++ quicksets/service/database.js
@@ -0,0 +1,29 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+
+const client = new MongoClient(url);
+const db = client.db('quicksets');
+
+// Collections
+const userCollection = db.collection('user');
+const workoutCollection = db.collection('workout');
+
+// Connect on startup
+async function connectToDatabase() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`Connected to DB`);
+  } catch (err) {
+    console.error("DB connection failed:", err);
+    process.exit(1);
+  }
+}
+
+connectToDatabase();
+
+module.exports = {
+  userCollection,
+  workoutCollection,
+};
